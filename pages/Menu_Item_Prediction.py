ea1584f6fb2ec3f2b3195b8a96ce0b3d53c176f7
Mayuresh Harihar <hariharmayuresh@gmail.com>
2024-04-13 17:34:32 +0530

    def main function to Menu_Item_Prediction.py

--- pages/Menu_Item_Prediction.py
+++ pages/Menu_Item_Prediction.py
@@ -24,17 +24,22 @@ COLUMN_DATA_PATH = os.path.join(dir_of_interest, "column_transformer.joblib")
 model = joblib.load(MENU_DATA_PATH)
 column_transformer = joblib.load(COLUMN_DATA_PATH)
 
-st.title("Menu Item Demand Prediction")
+# Create Streamlit app
+def main():
+    st.title("Menu Item Demand Prediction")
 
-# User inputs
-day = st.selectbox("Day of the Week:", ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
-holiday = st.selectbox("Holiday:", ["New Year's Day", 'NO', 'Weekend', 'Pongal', 'Makar Sankranti', 'Republic Day', "Valentine's Day", 'Holi', 'Gudi Padwa', "April Fools' Day", 'Ram Navami', 'Good Friday', 'Easter Sunday', 'Ambedkar Jayanti', 'Hanuman Jayanti', 'Buddha Purnima', 'May Day', "Mother's Day", 'Eid al-Fitr', "Father's Day", 'Bakri Id', 'Raksha Bandhan', 'Janmashtami', 'Independence Day','Ganesh Chaturthi', 'Onam', 'Hindi Diwas', 'Navratri'])
-weather = st.selectbox("Weather Condition:", ['Clear', 'Foggy', 'Rainy'])
+    # User inputs
+    day = st.selectbox("Day of the Week:", ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+    holiday = st.selectbox("Holiday:", ["New Year's Day", 'NO', 'Weekend', 'Pongal', 'Makar Sankranti', 'Republic Day', "Valentine's Day", 'Holi', 'Gudi Padwa', "April Fools' Day", 'Ram Navami', 'Good Friday', 'Easter Sunday', 'Ambedkar Jayanti', 'Hanuman Jayanti', 'Buddha Purnima', 'May Day', "Mother's Day", 'Eid al-Fitr', "Father's Day", 'Bakri Id', 'Raksha Bandhan', 'Janmashtami', 'Independence Day','Ganesh Chaturthi', 'Onam', 'Hindi Diwas', 'Navratri'])
+    weather = st.selectbox("Weather Condition:", ['Clear', 'Foggy', 'Rainy'])
 
-if st.button("Predict"):
-    input_data = pd.DataFrame({'Day of the week': [day], 'Holiday': [holiday], 'Weather Condition': [weather]})
-    input_data_transformed = column_transformer.transform(input_data)
-    predictions = model.predict_proba(input_data_transformed)
-    top_two_indices = predictions[0].argsort()[-2:][::-1]  # Get indices of top 2 predictions
-    top_two_menu_items = model.classes_[top_two_indices]
-    st.header(f"Predicted Menu Items: {top_two_menu_items[0]}, {top_two_menu_items[1]}")
+    if st.button("Predict"):
+        input_data = pd.DataFrame({'Day of the week': [day], 'Holiday': [holiday], 'Weather Condition': [weather]})
+        input_data_transformed = column_transformer.transform(input_data)
+        predictions = model.predict_proba(input_data_transformed)
+        top_two_indices = predictions[0].argsort()[-2:][::-1]  # Get indices of top 2 predictions
+        top_two_menu_items = model.classes_[top_two_indices]
+        st.header(f"Predicted Menu Items: {top_two_menu_items[0]}, {top_two_menu_items[1]}")
+        
+if __name__ == "__main__":
+    main()
